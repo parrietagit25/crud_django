15a4d5d5a50e3399f973df3c0bc4f90b079a6e0d
pedro <pedroarrieta25@hotmail.com>
2022-09-20 01:26:14 -0500

    crudrh terminado

--- libreria/urls.py
+++ libreria/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('peliculas', views.peliculas, name="peliculas"),
     path('peliculas/crear', views.crear_pelicula, name="crear"),
     path('peliculas/editar', views.editar_pelicula, name="editar"),
+    path('eliminar/<int:id>', views.eliminar_pelicula, name="eliminar"), # eliminar se pasa un parametro
+    path('peliculas/editar/<int:id>', views.editar_pelicula, name="editar"), # editar se pasa un parametro
 
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # conf imagenees
--- libreria/views.py
+++ libreria/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Pelicula
 from .forms import PeliculaForm
@@ -16,8 +16,21 @@ def peliculas(request):
     return render(request, 'peliculas/index.html', {'peliculas':peliculas})
 
 def crear_pelicula(request):
-    formulario = PeliculaForm(request.POST or None)
+    formulario = PeliculaForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save() # se guardan los datoss
+        return redirect('peliculas')
     return render(request, 'peliculas/crear.html', {'formulario': formulario})
 
-def editar_pelicula(request):
-    return render(request, 'peliculas/editar.html')
+def editar_pelicula(request, id):
+    pelicula = Pelicula.objects.get(id=id)
+    formulario = PeliculaForm(request.POST or None, request.FILES or None, instance=pelicula)
+    if formulario.is_valid and request.POST:
+        formulario.save()
+        return redirect('peliculas')
+    return render(request, 'peliculas/editar.html', {'formulario': formulario})
+
+def eliminar_pelicula(request, id):
+    pelicula = Pelicula.objects.get(id=id)
+    pelicula.delete() # se borra la pelicula
+    return redirect('peliculas')
